41903a03f6721f944b6e9c9af2759575387d7dfd
lbr0452000 <lbr0452000@naver.com>
2021-05-28 22:43:21 +0900

    Signin API 연동

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,23 @@
-export const signin = () => {
+import Cookies from 'universal-cookie';
+import axios from 'axios';
+
+export const signin = (username, password) => {
   return new Promise((resolve, reject) => {
-    resolve({
-      message: 'success',
-    });
+    axios({
+      method: 'post',
+      url: '/api/user/signin',
+      data: {
+        username: username,
+        password: password,
+      },
+    })
+      .then((res) => {
+        var cookies = new Cookies();
+        cookies.set('auth', res.data.body.token);
+        resolve(res.data.body.token);
+      })
+      .catch((err) => {
+        reject(err);
+      });
   });
 };
